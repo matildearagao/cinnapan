6214320c0169eb45d08e0125044f964f5a50aff5
matildehomem <matilde.a.s.homem@gmail.com>
2019-09-05 22:07:31 +0100

    teste

--- src/components/About/SectionInfo.js
+++ src/components/About/SectionInfo.js
@@ -10,7 +10,7 @@ export default function SectionInfo({cinnapanPhoto, styleClass}) {
                     <Img fluid={cinnapanPhoto} alt="" className="section-info__img-bw"/>
                 </div>
                 <div className="col-10 col-md-6">
-                    <h2 className="heading-main u-margin-bottom-small">Nossa historia</h2>
+                    <h2 className="heading-main u-margin-bottom-small">Nossa historia TETSTETETETET</h2>
                     <p className="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quod quisquam earum assumenda facilis provident, voluptatem voluptatum cum ipsa aliquam mollitia in impedit aut repellendus consequatur voluptatibus! Ducimus, commodi? Explicabo.</p>
                     <br />
                     <p className="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quod quisquam earum assumenda facilis provident, voluptatem voluptatum cum ipsa aliquam mollitia in impedit aut repellendus consequatur voluptatibus! Ducimus, commodi? Explicabo.</p>
